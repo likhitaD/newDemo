76745fe400c6906394f6d6e61e7b4381963bb3a8
likhitaD <deogadelikhita3108@gmail.com>
2019-07-23 20:32:41 +0530

    updated recently

--- src/main/java/jbk/A.java
+++ src/main/java/jbk/A.java
@@ -1,10 +1,12 @@
 package jbk;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class A {
 
 	
 	private WebDriver driver;
+	private WebElement fdhgitudh;
 	
 }
